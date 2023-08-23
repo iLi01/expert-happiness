ac49043be601eb337c33215f5dff7c3cf49106ff
[C[B[A[A[A[A[D[C[C[C[C[C[C[C[C[C[CiLi01[D[D[C[C[C[B[D[D[D[D[D[D[D[D[D[D[Cilinca.pricop100@gmail.com[C[B[C[C[C[C[C[C[C <ilinca.pricop100@gmail.com>
2023-08-23 16:54:45 +0100

    nothing works?????

--- nav_bar.js
+++ nav_bar.js
@@ -1,28 +1,14 @@
-// Select the button that will expand the navigation
-const navExpandButton = document.querySelector('#nav-expand-button')
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
 
-// select the element that is your navigation
-const nav = document.getElementById('mainNav')
-
-
-// create a function to tell the 
-// browser what to do when the button is clicked
-function handleClick() {
-    
-    // toggle a class of 'open' on the nav element, this will 
-    // change the visual style to `display: flex;` from `display: none;`
-    nav.classList.toggle('open')
-    
-
-    // get the state of the 'aria-expanded' attribute
-    const expandedState = nav.getAttribute('aria-expanded')
-        
-    // set the attribute of 'aria-expanded' to the opposite of what it currently is
-    nav.setAttribute('aria-expanded', !eval(expandedState))
-
-};
-
-
-// tell the browser to listen for a click event 
-// and to run "handleClick" when it is clicked
-navExpandButton.addEventListener('click', handleClick)
+// $(document).ready(function(){
+// 	$('menu_bars').click(function(){
+// 		$(this).toggleClass('open');
+// 	});
+// });
